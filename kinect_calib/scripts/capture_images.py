3aa1b5871a55123a7e1ca46a0501040774bca74c
awilkowski <ArturWilkowski@piap.pl>
2014-03-14 20:45:57 +0100

    Complete calibration

--- kinect_calib/scripts/capture_images.py
+++ kinect_calib/scripts/capture_images.py
@@ -23,7 +23,8 @@ from sensor_msgs.msg import Image
 # We do not use cv_bridge it does not support CompressedImage in python
 # from cv_bridge import CvBridge, CvBridgeError
 
-bridge = CvBridge() 
+bridge = CvBridge()
+
 
 def callbackrgb(data):
 	global bridge 
@@ -67,5 +68,64 @@ def listener():
 	cv2.namedWindow("Image window", 1)
 	rospy.spin()
         
+def listener1():
+#Initializations
+	global bridge 
+	rospy.init_node('listener', anonymous=True)
+	while not rospy.is_shutdown():
+		res = raw_input("Press enter to continue")
+#Get IR image
+		messageir = rospy.wait_for_message("/camera/ir/image_raw", Image)
+		print 'received image of type: "%s"' % messageir.header
+		print 'received image width = %s and height = %s' %(messageir.width, messageir.height)
+		# print 'data type: %s' % data.encoding	
+		try:
+			cv_image_ir = bridge.imgmsg_to_cv(messageir, "mono16")
+		except CvBridgeError, e:
+			print e
+		cv_image_ir = np.array(cv_image_ir, dtype=np.uint16)
+		cv_image_ir = cv2.convertScaleAbs(cv_image_ir, alpha=1.0)
+
+		#cv_image8 = cv2.medianBlur(cv_image8,3) 
+
+#Get RGB image
+		messagergb = rospy.wait_for_message("/camera/rgb/image_color", Image)
+		print 'received image of type: "%s"' % messagergb.header
+		print 'received image width = %s and height = %s' %(messagergb.width, messagergb.height)
+		try:
+			cv_image_rgb = bridge.imgmsg_to_cv(messagergb, "bgr8")
+		except CvBridgeError, e:
+			print e
+		cv_image_rgb = np.array(cv_image_rgb, dtype=np.uint8)
+
+#Locate corners
+		ir_corners_found, ir_corners = cv2.findChessboardCorners(cv_image_ir, (6,8), flags = cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_FAST_CHECK);
+		cv_image_ir_disp = cv_image_ir.copy() 
+		cv2.drawChessboardCorners(cv_image_ir_disp, (6,8), ir_corners, ir_corners_found) 
+		print 'IR corners found: %r' % ir_corners_found
+
+		rgb_corners_found, rgb_corners = cv2.findChessboardCorners(cv_image_rgb, (6,8), flags = cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_FAST_CHECK);
+		print 'RGB corners found: %r' % rgb_corners_found
+		cv_image_rgb_disp = cv_image_rgb.copy() 
+		cv2.drawChessboardCorners(cv_image_rgb_disp, (6,8), rgb_corners, rgb_corners_found) 
+
+#Display images
+		cv2.imshow("IR image", cv_image_ir_disp)
+		cv2.imshow("RGB image", cv_image_rgb_disp)
+
+		if ir_corners_found and rgb_corners_found:
+#Save images
+			filenameir = 'camerair-' + str(messageir.header.stamp.secs) + '-' + str(messageir.header.stamp.nsecs) + '.png'	
+			print 'Saving ' + filenameir
+			cv2.imwrite(filenameir, cv_image_ir)
+
+			filenamergb = 'camerargb-' + str(messageir.header.stamp.secs) + '-' + str(messageir.header.stamp.nsecs) + '.png'	
+			print 'Saving ' + filenameir
+			cv2.imwrite(filenamergb, cv_image_rgb)
+		
+		cv2.waitKey(1000)
+
 if __name__ == '__main__':
-    listener()
+	try:
+		listener1() 
+	except rospy.ROSInterruptException: pass
---
+++ kinect_calib/scripts/publish_images.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+PKG = 'kinect_calib' # this package name
+import roslib; roslib.load_manifest(PKG)
+
+import rospy
+
+# Python libs
+import sys, time
+
+# numpy and scipy
+import numpy as np
+import scipy
+from scipy.ndimage import filters
+from cv_bridge import CvBridge, CvBridgeError
+
+# OpenCV
+import cv2
+import cv
+
+# Ros Messages
+from sensor_msgs.msg import Image
+
+import glob 
+
+bridge = CvBridge()
+
+def talker():
+	pubir = rospy.Publisher('camera/ir_virtual', Image)
+	pubrgb = rospy.Publisher('camera/rgb_virtual', Image)
+	rospy.init_node('talker', anonymous=True)
+	r = rospy.Rate(1) # 1hz
+	while not rospy.is_shutdown():
+		filenames = glob.glob('camerair*')
+		filenames.sort() #Some files have shorter usec part so the sorting is not perfect 
+		for filenameir in filenames:
+			if rospy.is_shutdown(): break #Stop after Ctrl-C
+			filenamergb = 'camerargb-' + filenameir[9:]
+
+			#Publish IR image	
+			print 'Publishing ' + filenameir 
+			cv_image_ir = cv2.imread(filenameir, flags=cv.CV_LOAD_IMAGE_UNCHANGED) ;	
+			cv_image_ir = cv.fromarray(cv_image_ir) #Falling back to cv for conversion
+			try: 
+				messageir = bridge.cv_to_imgmsg(cv_image_ir, "mono8")
+			except CvBridgeError, e: print e
+			messageir.header.frame_id = '' #for now (works for calibration anyway)
+			messageir.header.stamp = rospy.Time.now() 
+			pubir.publish(messageir)
+
+			#Publish RGB image
+			print 'Publishing ' + filenamergb 
+			cv_image_rgb = cv2.imread(filenamergb, flags=cv.CV_LOAD_IMAGE_UNCHANGED) ;	
+			cv_image_rgb = cv.fromarray(cv_image_rgb) #Falling back to cv for conversion
+			try: 
+				messagergb = bridge.cv_to_imgmsg(cv_image_rgb, "bgr8")
+			except CvBridgeError, e: print e
+			messagergb.header.frame_id = '' #for now
+			messagergb.header.stamp = messageir.header.stamp
+			pubrgb.publish(messagergb)
+			
+			r.sleep()
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException: pass

ca8b02db6e53ea4588208943a5a7b4e5cd9d3977
awilkowski <ArturWilkowski@piap.pl>
2014-09-12 22:09:04 +0200

    Scene cloud residing in an octree + octree frustum culling

--- surfel_mapper/src/surfel_mapper_node.cpp
+++ surfel_mapper/src/surfel_mapper_node.cpp
@@ -3,6 +3,7 @@
 #include "sensor_msgs/PointCloud2.h"
 #include "pcl_conversions/pcl_conversions.h"
 #include <Eigen/Geometry>
+#include <limits.h>
 
 #include <pcl/common/common_headers.h>
 #include <pcl/filters/filter_indices.h>
@@ -13,13 +14,14 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl/visualization/common/common.h>
 #include <pcl/octree/octree.h>
+#include <pcl/octree/octree_impl.h>
 
 #define DMAX 0.05f
 #define CLOUD_WIDTH 640
 #define CLOUD_HEIGHT 480
 #define MIN_KINECT_DIST 0.8 
 #define MAX_KINECT_DIST 4.0
-#define OCTREE_RESOLUTION 0.1
+#define OCTREE_RESOLUTION 0.2 
 
 struct SensorPose {
 	public:
@@ -78,13 +80,18 @@ bool getSensorPosition(const ros::Time &time_stamp, SensorPose &sensor_pose)
 
 /**
  * Gets interpolated -z- value at specified (not necesserily integer) position in organized cloud
+ * negative z - denotes sampling out of depth image bounds, nan - denotes invalid reading at given position of the organized cloud
  */
 float getZAtPosition(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud, float u, float v)
 {
 	//Use a simplest nearest neighbor approach now (todo: upgrade to bilinear interpolation)
-	if (u <= -0.5 || v <= -0.5 || u >= cloud->width - 0.5 || v >= cloud->height - 0.5) //<= >= instead of < > - easier subsequent modulo search
-		return std::numeric_limits<float>::quiet_NaN () ;
-	else {
+	if (u <= -0.5 || v <= -0.5 || u >= cloud->width - 0.5 || v >= cloud->height - 0.5) { //<= >= instead of < > - easier subsequent modulo search 
+		/*float xxx = 1.0 ;
+		if (u < -0.5 - xxx || u > cloud->width + xxx || v < -0.5 - xxx || v > cloud->height + xxx)
+			ROS_INFO("Out of bounds (u, v) = (%f, %f)", u, v) ;*/
+		return -1.0f ;	
+		//return std::numeric_limits<float>::quiet_NaN () ;
+	} else {
 		//Now perform NN interpolation
 		uint32_t i = static_cast<int>(v + 0.5) ;
 		uint32_t j = static_cast<int>(u + 0.5) ;
@@ -344,7 +351,7 @@ void addPointCloudToScene(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud)
 			//std::cout << i << ": (" << j - u << ", " << i - v << ")" << std::endl ;
 			
 			float zscan = getZAtPosition(cloud, u, v) ;
-			if (!std::isnan(zscan)) {
+			if (!std::isnan(zscan)) { //not valid - check the newer function...
 				if (fabs(zscan - point.z) <= DMAX) { 
 					//We have a surfel-scan match, we may update the surfel here... (TODO)
 					markScanAsCovered(scan_covered, u, v) ; 
@@ -392,6 +399,172 @@ void addPointCloudToScene(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud)
 	ROS_INFO("Cloud downsampling time(s): [%.6lf]", (stop - start).toSec()) ;
 }
 
+inline void transformPointAffine(pcl::PointXYZRGB &point_in, pcl::PointXYZRGB &point_out, Eigen::Matrix4d transform)
+{
+	point_out = point_in ;
+	point_out.x = static_cast<float> (transform (0, 0) * point_in.x + transform (0, 1) * point_in.y + transform (0, 2) * point_in.z + transform (0, 3));
+	point_out.y = static_cast<float> (transform (1, 0) * point_in.x + transform (1, 1) * point_in.y + transform (1, 2) * point_in.z + transform (1, 3));
+	point_out.z = static_cast<float> (transform (2, 0) * point_in.x + transform (2, 1) * point_in.y + transform (2, 2) * point_in.z + transform (2, 3));
+}
+
+void addPointCloudToScene1(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud)
+{
+	//Filter points too close and too far
+	ros::Time start = ros::Time::now();
+	filterCloudByDistance(cloud) ;
+	ros::Time stop = ros::Time::now() ;
+	ROS_INFO("Filtering points outside reliable Kinect scope time (s): [%.6lf]", (stop - start).toSec()) ;
+
+	//Testing cloud frustum
+	//testCloud(cloud) ;
+
+	double alpha = 518.930578 ; //fx
+	double cx = 323.483756 ;
+	double beta = 517.211658 ; //fy
+	double cy = 260.384697 ;
+
+	double width = 640 ;
+	double height = 480 ;
+
+	//Compute a projection view matrix
+	double f = MAX_KINECT_DIST ; 
+	double n = MIN_KINECT_DIST ;
+
+	Eigen::Matrix4d viewMatrix ;
+	viewMatrix << cloud->sensor_orientation_.toRotationMatrix().cast<double>(), cloud->sensor_origin_.topRows<3>().cast<double>(), 0.0, 0.0, 0.0, 1.0 ;
+	viewMatrix = viewMatrix.inverse() ;
+	//Eigen::Matrix4f viewMatrixf = viewMatrix.cast<float>() ;
+
+	Eigen::Matrix4d projectionMatrix ; 
+	projectionMatrix << 2 * alpha / width, 0.0, 2 * cx / width - 1.0, 0.0,
+				0.0, 2 * beta / height, 2 * cy / height - 1.0, 0.0,
+				0.0, 0.0, (f + n) / (f - n), -2 * f * n / (f - n),
+				0.0, 0.0, 1.0, 0.0 ;
+	//std::cout << "View matrix: " << std::endl << viewMatrix << std::endl ;
+	//std::cout << "Projection matrix: " << std::endl << projectionMatrix << std::endl ;
+
+	Eigen::Matrix4d projectionViewMatrix = projectionMatrix * viewMatrix ;
+
+	//Computing frustum
+	double frustum[24] ;
+	pcl::visualization::getViewFrustum(projectionViewMatrix, frustum) ;
+
+	//Clean-up the scan covered array
+	static char scan_covered[CLOUD_HEIGHT][CLOUD_WIDTH] ;
+	memset(scan_covered, 0, sizeof(scan_covered[0][0]) * CLOUD_HEIGHT * CLOUD_WIDTH);
+
+	unsigned int nsurfels_updated = 0 ;
+	unsigned int octree_nodes_visited = 0 ;
+	unsigned int surfels_inside_octree_frustum = 0 ;
+	unsigned int surfels_projected_on_sensor = 0 ;
+	unsigned int nscan_too_close = 0 ;
+	unsigned int nscan_too_far = 0 ;
+	unsigned int nsurfels_invalid_reading = 0 ;
+	start = ros::Time::now();
+
+	//Iterate Octree in a depth-first manner
+	unsigned int acceptBelowDepth = UINT_MAX ;
+	pcl::octree::OctreePointCloud<pcl::PointXYZRGB>::DepthFirstIterator it = octree.depth_begin() ;
+	const pcl::octree::OctreePointCloud<pcl::PointXYZRGB>::DepthFirstIterator it_end = octree.depth_end();
+	while(it != it_end) {
+		octree_nodes_visited++ ;
+		unsigned int current_depth = it.getCurrentOctreeDepth() ;
+
+		//Cancel acceptBelowDepth if we went above a child branch that is completely in a frustum
+		if (current_depth <= acceptBelowDepth)
+			acceptBelowDepth = UINT_MAX ;
+
+		//Compute frustum if necessary
+		int frustum_result ;
+		if (current_depth > acceptBelowDepth)  
+			frustum_result = pcl::visualization::PCL_INSIDE_FRUSTUM ;
+		else {
+			Eigen::Vector3f min_bb, max_bb ;
+			octree.getVoxelBounds(it, min_bb, max_bb) ;	
+			frustum_result = pcl::visualization::cullFrustum(frustum, min_bb.cast<double>(), max_bb.cast<double>()) ; 
+			if (frustum_result == pcl::visualization::PCL_INSIDE_FRUSTUM)
+				acceptBelowDepth = it.getCurrentOctreeDepth() ; //We may mark that all nodes below will be automatically accepted
+		}
+	
+		if (frustum_result == pcl::visualization::PCL_OUTSIDE_FRUSTUM) 
+			it.skipChildVoxels() ;
+		else if (it.isLeafNode()) {
+			//Transform and update all points in a leaf
+			pcl::octree::OctreeContainerPointIndices& container = it.getLeafContainer();
+			std::vector<int> pointIndices ; 
+			container.getPointIndices (pointIndices);
+			pcl::PointXYZRGB pointTrans ;
+			for (int i = 0; i < pointIndices.size() ; i++)  {
+				transformPointAffine(cloudScene->points[pointIndices[i]], pointTrans, viewMatrix) ;
+				float xp = pointTrans.x / pointTrans.z ;
+				float yp = pointTrans.y / pointTrans.z ;
+				float u = alpha * xp + cx ;
+				float v = beta * yp + cy ;
+				float zscan = getZAtPosition(cloud, u, v) ;
+				surfels_inside_octree_frustum++ ;
+				if (std::isnan(zscan) || zscan >= 0.0f) //in both cases we hit image plane
+					surfels_projected_on_sensor++ ;
+				if (!std::isnan(zscan) && zscan >= 0.0f) {
+					//surfels_projected_on_sensor++ ;
+					if (fabs(zscan - pointTrans.z) <= DMAX) { 
+						//We have a surfel-scan match, we may update the surfel here... (TODO)
+						markScanAsCovered(scan_covered, u, v) ; 
+						nsurfels_updated++ ;
+					} else if (zscan - pointTrans.z > DMAX) {
+						//The observed point is behing the surfel, we may either remove the observation or the surfel (depending e.g. on the confidence)
+						markScanAsCovered(scan_covered, u, v) ;
+						nscan_too_far++ ;
+					} else
+						nscan_too_close++ ;
+				} else nsurfels_invalid_reading++ ;
+			}
+		}		
+		it++ ;
+	}
+	
+	stop = ros::Time::now() ;
+	ROS_INFO("Surfels inside octree frustum [%d]", surfels_inside_octree_frustum) ;
+	ROS_INFO("Surfels projected on sensor plane [%d]", surfels_projected_on_sensor) ;
+	ROS_INFO("Projected/inside frustum [%f%%]", double(surfels_projected_on_sensor)/surfels_inside_octree_frustum * 100) ;
+	ROS_INFO("Outside frustum/total points [%f%%]", double(cloudScene->width - surfels_inside_octree_frustum) / cloudScene->width * 100) ;
+	ROS_INFO("Octree nodes visited during update [%d]", octree_nodes_visited) ;
+	ROS_INFO("Surfel update time (s): [%.6lf]", (stop - start).toSec()) ;
+	ROS_INFO("Surfels updated [%d]", nsurfels_updated) ;
+	ROS_INFO("Scan too far for surfel update [%d]", nscan_too_far) ;
+	ROS_INFO("Scan too close for surfel update [%d]", nscan_too_close) ;
+	ROS_INFO("Surfels without matching reading [%d]", nsurfels_invalid_reading) ;
+
+	start = ros::Time::now() ;
+	//Perform surfel-addition step
+	//Create temporary point cloud (of surfels) to be concatenated with the scene cloud (TODO we may do without intermediary cloud, perhaps faster)	
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudTemp(new pcl::PointCloud<pcl::PointXYZRGB>) ;
+	pcl::copyPointCloud(*cloud, *cloudTemp) ;
+
+	unsigned int surfels_added = 0 ;
+	//Update surfel data in the cloud to add and remove covered measurements
+	for (uint32_t i = 0; i < cloud->height ; i++) 
+		for (uint32_t j = 0; j < cloud->width ; j++) { 
+			if (!scan_covered[i][j]) {
+				//Add a new point to the scene cloud (and the associated octree)
+				pcl::PointXYZRGB &point = (*cloud)(j, i) ;
+				if (pcl::isFinite(point)) {
+					octree.addPointToCloud(point, cloudScene) ;
+					surfels_added++ ;
+					//TODO Some other (more complex) processing is required here...
+				}
+			}
+		}
+	stop = ros::Time::now() ;
+	ROS_INFO("Surfel addition time (s): [%.6lf]", (stop - start).toSec()) ;
+	ROS_INFO("Surfels added [%d]", surfels_added) ;
+	ROS_INFO("cloud_scene size: [%d]", cloudScene->width) ;
+
+	//Now downsample scene cloud
+	start = ros::Time::now() ;
+	downsampleSceneCloud() ;
+	stop = ros::Time::now() ;
+	ROS_INFO("Cloud downsampling time(s): [%.6lf]", (stop - start).toSec()) ;
+}
 
 void processCloudMsgQueue()
 {
@@ -412,8 +585,8 @@ void processCloudMsgQueue()
 			cloud->sensor_orientation_ = sensor_pose.orientation ;
 
 			//Add cloud to the map
-			addPointCloudToScene(cloud) ;
-			ROS_INFO("Adding point cloud [%d, %d]", msg->header.stamp.sec, msg->header.stamp.nsec) ;
+			addPointCloudToScene1(cloud) ;
+			ROS_INFO("-------------->Adding point cloud [%d, %d]", msg->header.stamp.sec, msg->header.stamp.nsec) ;
 			ROS_INFO("Sensor position data: [%f, %f, %f, %f] ", cloud->sensor_origin_.x(), cloud->sensor_origin_.y(), cloud->sensor_origin_.z(), cloud->sensor_origin_.w()) ;
 			ROS_INFO("Sensor orientation data: [%f, %f, %f, %f] ", cloud->sensor_orientation_.x(), cloud->sensor_orientation_.y(), cloud->sensor_orientation_.z(), cloud->sensor_orientation_.w()) ;
 
@@ -459,6 +632,9 @@ int main(int argc, char **argv)
 	ros::Publisher map_pub = n.advertise<sensor_msgs::PointCloud2>("surfelmap", 5);
 
 	ros::Rate r(2) ;
+
+	octree.setInputCloud(cloudScene) ;
+
 	while(ros::ok()) {
 		ros::spinOnce();
 		processCloudMsgQueue() ;
